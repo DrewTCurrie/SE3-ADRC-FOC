c5db7a607799e71dd3430e12e3dc6e4b927193e6
drewtcurrie <drewtcurrie@gmail.com>
2026-08-17 16:15:49 -0600

    Added useful enviorment variables to project

--- enviormentVars.sh
+++ enviormentVars.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# Adding project root as an export for easy navigation
+export PROOT=/opt/Xilinx/Projects/SE3-ADRC-FOC/
+echo -e "Set Project Root as $PROOT"
+echo -e "\n"
+# NFS root and Kernel build enviornment variables
 export NFSROOT=/srv/nfs/shared/petalinux-nfs
 export KVER=$(cat $NFSROOT/lib/modules/*/kernel/.. 2>/dev/null; ls $NFSROOT/lib/modules)
 echo -e "Enviornment variables set:"
 echo -e "\t NFSROOT=$NFSROOT"
-echo -e "\t Board Kernel version: $KVER"
+echo -e "\t Target Kernel verison: $KVER"
